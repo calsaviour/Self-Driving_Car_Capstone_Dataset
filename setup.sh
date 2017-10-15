f029c79aee7730161b7af71ab55f150478978396
Calvin Low <calsaviour@gmail.com>
2017-10-15 17:22:49 +0800

    Add setup steps for creating directory

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-DATA_DIR=""
+HOME_DIR=${HOME}
+PROJECT_DIR="Self-Driving_Car_Capstone_Dataset"
+WORKING_DIR=${HOME_DIR}/${PROJECT_DIR}
+DATA__TRAIN_DIR="data/simulator/train"
+SIMULATOR_DIR=${WORKING_DIR}/${DATA_DIR}
 
-mkdir -p ./data/simulator/train/red
-mkdir -p ./data/simulator/train/green
+mkdir -p ${SIMULATOR_DIR}/redd
 
