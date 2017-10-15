f9e8fd8c91e48f35d705206d7a6f98da6fe7fa69
Calvin Low <calsaviour@gmail.com>
2017-10-15 09:27:57 +0000

    Add simulator model for traffic light using CNN Accurary is 65%

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 HOME_DIR=${HOME}
 PROJECT_DIR="Self-Driving_Car_Capstone_Dataset"
 WORKING_DIR=${HOME_DIR}/${PROJECT_DIR}
-DATA__TRAIN_DIR="data/simulator/train"
-SIMULATOR_DIR=${WORKING_DIR}/${DATA_DIR}
+DATA_TRAIN_DIR="data/simulator/train"
+SIMULATOR_DIR=${WORKING_DIR}/${DATA_TRAIN_DIR}
 
-mkdir -p ${SIMULATOR_DIR}/redd
+echo ${SIMULATOR_DIR}
+##mkdir -p ${SIMULATOR_DIR}/redd
 
