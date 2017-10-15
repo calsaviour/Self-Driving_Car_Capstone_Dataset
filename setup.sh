71c13dcb5f165cb88a54a604b3fa2f49169d092a
Calvin Low <calsaviour@gmail.com>
2017-10-15 17:33:49 +0800

    Add steps to create directory for simulator train and validation dataset

--- setup.sh
+++ setup.sh
@@ -4,8 +4,18 @@ HOME_DIR=${HOME}
 PROJECT_DIR="Self-Driving_Car_Capstone_Dataset"
 WORKING_DIR=${HOME_DIR}/${PROJECT_DIR}
 DATA_TRAIN_DIR="data/simulator/train"
-SIMULATOR_DIR=${WORKING_DIR}/${DATA_TRAIN_DIR}
+SIMULATOR_TRAIN_DIR=${WORKING_DIR}/${DATA_TRAIN_DIR}
 
-echo ${SIMULATOR_DIR}
-##mkdir -p ${SIMULATOR_DIR}/redd
+DATA_VALIDATION_DIR="data/simulator/validation"
+SIMULATOR_VALIDATION_DIR=${WORKING_DIR}/${DATA_VALIDATION_DIR}
+
+## Create directory for training dataset
+mkdir -p ${SIMULATOR_TRAIN_DIR}/red
+mkdir -p ${SIMULATOR_TRAIN_DIR}/yellow
+mkdir -p ${SIMULATOR_TRAIN_DIR}/green
+
+## Create directory for validation dataset
+mkdir -p ${SIMULATOR_VALIDATION_DIR}/red
+mkdir -p ${SIMULATOR_VALIDATION_DIR}/yellow
+mkdir -p ${SIMULATOR_VALIDATION_DIR}/green
 
